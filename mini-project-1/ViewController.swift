ee52f6cf004b91aa2becc69d2b8278353499df0b
Hana Stauss <hanastauss@icloud.com>
2020-07-28 09:34:33 -0700

    Commit 1

--- mini-project-1/ViewController.swift
+++ mini-project-1/ViewController.swift
@@ -10,11 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var funFact: UILabel!
+
+    @IBAction func submitButtonTapped(_ sender: UIButton) {
+        
+        funFact.text = "I have a twin I was born in London, I have a dual citizen ship, I'm going to college next year, and I love KWK! <3"
+
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
 }
 
